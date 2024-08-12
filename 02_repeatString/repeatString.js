5ca96d6a8cac0064e8d8ed42801f822cfb288aff
Nagul75 <nagulk697@gmail.com>
2024-08-12 15:48:26 +0530

    idk

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let totalString= "";
+    if (num < 0)
+    {
+        return "ERROR"
+    }
+    for(let i = 0; i < num; i++)
+    {
+        totalString += str
+    }
+    return totalString
 };
 
 // Do not edit below this line
