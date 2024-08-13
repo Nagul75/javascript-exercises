4f9a576aed750c91da240faafb474cf04aba423a
Nagul75 <nagulk697@gmail.com>
2024-08-13 18:16:48 +0530

    pffft

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof(num) == String) num = parseInt(num);
+    if (num < 0) return "OOPS";
+    let fibonacci = [0, 1, 1];
+    let f1 = fibonacci[1], f2 = fibonacci[2];
+    for(let i = 3; i <= num; i++)
+    {
+        let sum = f1 + f2;
+        fibonacci.push(sum);
+        f1 = f2;
+        f2 = sum;
+    }
+    return fibonacci[num];
 };
 
 // Do not edit below this line
