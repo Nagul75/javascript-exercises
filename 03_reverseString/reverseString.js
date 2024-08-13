5207d7b8bb77c4724def39b616dd78c6c4041f14
Nagul75 <nagulk697@gmail.com>
2024-08-13 14:34:18 +0530

    commit!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let charArray = str.split('');
+    reversedString = ""
+    for(let i = charArray.length-1; i >= 0; i--)
+    {
+        reversedString += charArray[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
