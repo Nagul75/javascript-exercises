15f6d4ebf8c5bcafdd4f54525415882acd0216bf
Nagul75 <nagulk697@gmail.com>
2024-08-13 18:24:19 +0530

    has

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
 const getTheTitles = function() {
-
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+    ]
+    let arrBooks = books.map(a => a.title);
+    return arrBooks;
 };
 
 // Do not edit below this line
